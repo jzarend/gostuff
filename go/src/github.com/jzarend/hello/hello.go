f6efbc8b8d9a86de929a8947947e605d297971c9
Jeff Zarend <jzarend@yahoo.com>
2018-02-24 12:30:59 -0500

    add a library

--- go/src/github.com/jzarend/hello/hello.go
+++ go/src/github.com/jzarend/hello/hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+        "fmt"
+        "github.com/jzarend/stringutil"
+)
 
 func main() {
-    fmt.Printf("Hello World\n")
+    fmt.Printf("Hello World!\n")
+    fmt.Printf(stringutil.Reverse("Hello World!")+"\n")
+    fmt.Printf(stringutil.Reverse("!dlroW olleH")+"\n")
 }
